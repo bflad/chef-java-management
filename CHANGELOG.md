bfbfe3675807ecfcbcd33a45174fda96e8b110c1
Brian Flad <bflad417@gmail.com>
2012-11-05 21:40:45 -0500

    Bump to v0.1.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## v0.1.1
+
+* Added basic minitests
+
 ## v0.1.0
 
-Initial beta release with Java SNMP and JMX management.
+* Initial beta release with Java SNMP and JMX management.
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email  "bflad@wharton.upenn.edu"
 license           "Apache 2.0"
 description       "Java Management Cookbook"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "0.1.0"
+version           "0.1.1"
 recipe            "java-management", "Java Management"
 
 %w{ amazon centos redhat scientific }.each do |os|
