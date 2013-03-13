cbb3edfc58557e25d349b5ba1f3731dd7c2e3829
Brian Flad <bflad417@gmail.com>
2013-03-13 08:36:38 -0400

    Bump to version 1.0.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email  "bflad@wharton.upenn.edu"
 license           "Apache 2.0"
 description       "Java Management Cookbook"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "0.3.2"
+version           "1.0.0"
 recipe            "java-management", "Empty recipe"
 recipe            "java-management::management", "Java Management"
 recipe            "java-management::truststore", "Java Truststore Management"
